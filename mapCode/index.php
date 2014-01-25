6c0f90d0358307b9385aff22ad59a010c25b5f73
Austin Wells <acwells@smu.edu>
2014-01-25 13:32:30 -0800

    Updated heat map.

--- mapCode/index.php
+++ mapCode/index.php
@@ -7,21 +7,75 @@
         height: 600px;
       }
     </style>
-    <script src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>
-    <script>
+    <script src="http://maps.googleapis.com/maps/api/js?libraries=geometry,visualization&sensor=false"></script>
+     <script>
+
+      var geocoder;
+      var map;
+      var heatmapData = []; //heat map
       function initialize() {
-        var map_canvas = document.getElementById('map_canvas');
-        var map_options = {
-          center: new google.maps.LatLng(39.8282, -98.5795),
+        geocoder = new google.maps.Geocoder();
+        var latlng = new google.maps.LatLng(39.8282, -98.5795);
+        var mapOptions = {
           zoom: 4,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
+          center: latlng
         }
-        var map = new google.maps.Map(map_canvas, map_options)
+        map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
       }
-      google.maps.event.addDomListener(window, 'load', initialize);
+
+  function codeAddress() {
+   
+    var address = document.getElementById("address").value;
+    geocoder.geocode( { 'address': address}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        // var weightedLoc ={
+        //  location: results[0].geometry.location,
+        //  weight: 0.5
+        //  }//heat map
+        // heatmapData.push(weightedLoc); //push info to heat map 
+        // var heatmap = new google.maps.visualization.HeatmapLayer({
+        //   data: heatmapData,
+        //   dissipating: false,
+        //   radius: 2,
+        //   opacity: 0.3,
+        //   map: map
+        // });
+         map.setCenter(results[0].geometry.location);
+        var marker = new google.maps.Marker({
+            map: map,
+            position: results[0].geometry.location,
+            icon: getCircle()
+        }); 
+       
+      } else {
+        alert("Geocode was not successful for the following reason: " + status);
+      }
+    });
+
+     
+  }
+
+
+
+  function getCircle() {
+  var circle = {
+    path: google.maps.SymbolPath.CIRCLE,
+    fillColor: 'red',
+    fillOpacity: .2,
+    scale: 30,
+    strokeColor: 'white',
+    strokeWeight: .5
+  };
+  return circle;
+}
+      
     </script>
   </head>
-  <body>
-    <div id="map_canvas"></div>
-  </body>
+  <body onload="initialize()">
+ <div id="map-canvas" style="width: 1000px; height: 600px;"></div>
+  <div>
+    <input id="address" type="textbox" value="Sydney, NSW">
+    <input type="button" value="Encode" onclick="codeAddress()">
+  </div>
+</body>
 </html>
